585c4e1cbed9e61f87d54b888c0276cbdfd49e66
Rodrigo Solano <rodrigosolano014@gmail.com>
2021-06-03 16:10:04 -0500

    Proyecto finalizado

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+const ContenedorHeader = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+const TextoHeader = styled.header`
+  font-size: 2rem;
+  margin: 0;
+  font-family: "Slabo 27px", size;
+  text-align: center;
+`;
+
+function Header({ titulo }) {
+  return (
+    <ContenedorHeader>
+      <TextoHeader>{titulo}</TextoHeader>
+    </ContenedorHeader>
+  );
+}
+
+export default Header;
